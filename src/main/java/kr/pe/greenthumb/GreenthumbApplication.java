e294a6f3e7f0c566847819ee6e6c3b9e4bef035d
zyan <bingbong-party>
2021-11-24 18:12:19 +0900

    initial commit

---
+++ src/main/java/kr/pe/greenthumb/GreenthumbApplication.java
@@ -0,0 +1,15 @@
+package kr.pe.greenthumb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenthumbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenthumbApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/kr/pe/greenthumb/GreenthumbApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.greenthumb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenthumbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
