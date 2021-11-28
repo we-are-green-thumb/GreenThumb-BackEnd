019a82934e391ba8cd79e730ca24328e15432963
do-oni <dxxneo@gmail.com>
2021-11-28 19:37:50 +0900

    [Feat] Add  Login api

---
+++ src/main/java/kr/pe/greenthumb/config/auth/SecurityConfig.java
@@ -0,0 +1,39 @@
+package kr.pe.greenthumb.config.auth;
+
+import kr.pe.greenthumb.domain.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private final kr.pe.greenthumb.config.auth.CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+//                .csrf().disable()
+//                .headers().frameOptions().disable()
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+//                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+//                .anyRequest().authenticated()
+//                .and()
+                .logout()
+                .logoutSuccessUrl("/index.html")
+                .and()
+                .oauth2Login()
+                .defaultSuccessUrl("/index.html")
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+}
